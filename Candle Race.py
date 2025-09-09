0abb62cef1383eacc2b4071b88d8c13dd9990f35
Gusbc <gchri21@student.sdu.dk>
2025-09-09 21:31:30 +0200

    template

--- Candle Race.py
+++ Candle Race.py
@@ -1,3 +1,43 @@
-from roar_net_api import *
+from __future__ import annotations
 
-class Problem(self):
+from typing import final
+
+from roar_net_api.operations import *
+
+
+
+# ---------------------------------- Problem --------------------------------
+@final
+class Problem(SupportsEmptySolution, SupportsConstructionNeighbourhood):
+    def __init__(self, villages):
+        self.villages = villages
+        self.distances = calc_distances(villages)
+
+
+# ---------------------------------- Solution --------------------------------
+@final
+class Solution():
+    def __init__(self):
+        NotImplemented
+
+
+# ------------------------------- Neighbourhood ------------------------------
+@final
+class AddNeighbourhood(SupportsMoves[Solution, "AddMove"]):
+    def __init__(self):
+        NotImplemented
+
+# ----------------------------------- Moves -----------------------------------
+@final
+class AddMove(SupportsApplyMove[Solution], SupportsLowerBoundIncrement[Solution]):
+    def __init__(self):
+        NotImplemented
+
+
+
+
+
+## Helpers ##
+
+def calc_distances(villages):
+    NotImplemented
